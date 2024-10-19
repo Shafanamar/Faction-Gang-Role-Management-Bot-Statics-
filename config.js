0d1eb3538e7996c6acd3a1b5124aa359783a5ad2
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-19 11:13:21 +0530

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    token: '',
+    token: 'MTI4NzQ2NTQ5OTkxOTI1Nzc0Mg.GDzG06.Y9zJ3Lafre2cLOo2KBz1o9aay7qteqHGkOOxcY',
     clientId: '1287465499919257742',
     guildId: '124054615657704322430164' // optional, for testing in a specific guild
 };
